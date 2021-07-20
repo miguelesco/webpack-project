20fbe1f435d276056c7132d36c4b4316be674321
miguelesco <miuelangel1999@gmail.com>
2021-07-20 19:30:13 -0400

    remove drag functions

--- src/app/modules/list.js
+++ src/app/modules/list.js
@@ -1,5 +1,4 @@
 import checkLabels from './utilities.js';
-// import dragHanddler from './dragItems.js';
 import storage from './storage.js';
 
 const listWrapper = document.querySelector('.list');
@@ -32,57 +31,7 @@ const listRender = () => {
   render();
   checkLabels();
   console.log(storage.get())
-  dragHanddler();
 };
-
-
-function dragHanddler () {
-  Array.from(listWrapper.children).forEach((element, i) => {
-  
-    const dragabbleBtn = element.children[2];
-    dragabbleBtn.addEventListener('mousedown', () => {
-      element.setAttribute("draggable", true);
-    });
-
-    element.addEventListener("dragstart", (event) => {
-      console.log(event.dataTransfer)
-      event.dataTransfer.setData('index', i);
-    });
-
-    element.addEventListener('drop', (event) => {
-      const draggedIndex = event.dataTransfer.getData("index"); 
-      const dropIndex = i; 
-      
-      swap(draggedIndex, dropIndex);
-    });
-
-    element.addEventListener("dragover", (event) => {
-      event.preventDefault();
-    });
-  });
-}
-
-function swap(draggedIndex, dropIndex) {
-  // We get the current items
-  list = storage.get();
-  list.forEach(element => {
-    const dragged = element[draggedIndex];
-    const drop = element[dropIndex];
-
-    // We swap their positions
-    element[draggedIndex] = drop;
-    element[dropIndex] = dragged;
-    
-    // Update their indexes to reflect their new positions
-    element.index = dropIndex;
-    element.index = draggedIndex;
-  })
-
-  storage.set('elements', JSON.stringify(list));
-
-  // Then finally update the display
-  listRender();
-}
 
 input.addEventListener('keyup', (event) => {
   event.preventDefault();
